43e3fdafa3bab4685077f176e19ec7bd091cfa5b
Rohan Walia <rohanwalia@Rohans-MacBook-Pro.local>
2019-02-13 13:19:05 +0800

    Working Logic for the solution

--- exercises/character-input/code/character-input.py
+++ exercises/character-input/code/character-input.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
+import datetime
 age = input("Hi There!! Enter your age : ")
 age = int(age)
-print("Age of person is")
-print(age)
+yearsToHunderd = 100 - age
+currentYear = datetime.datetime.now()
+centuryYear = currentYear.year + yearsToHunderd
+print("You will be 100 years old in Year :{:d} ".format(centuryYear))
+
